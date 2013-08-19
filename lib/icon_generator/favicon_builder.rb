30043f05b3721370e3db35ce7c25d09b369f9cf3
Adam Bowen <adambowen@group360.com>
2013-08-19 12:22:53 -0500

    Correct output of transparent pngs

--- lib/icon_generator/favicon_builder.rb
+++ lib/icon_generator/favicon_builder.rb
@@ -4,7 +4,7 @@ module IconGenerator
 
         def build(source, destination)
             new_image = "#{destination}/favicon.ico"
-            %x[convert '#{source}' -resize 16x16! -flatten -colors 256 -background transparent #{new_image}]
+            %x[convert '#{source}' -resize 16x16 -alpha on #{new_image}]
             validate_file_status new_image
             puts Thor::Shell::Color.new.set_color("Built #{new_image}", :green)
         end
